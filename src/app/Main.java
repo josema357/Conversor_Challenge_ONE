dffdca9dc230350fa16d5a747ba375f5d65b3fd4
Jose Manuel <j.ochoa317@gmail.com>
2023-03-24 20:06:38 -0500

    Building MainWindow & MainWindowController

---
+++ src/app/Main.java
@@ -0,0 +1,22 @@
+package app;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class Main extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("../resources/MainWindow.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 600, 400);
+        stage.setTitle("Converter");
+        stage.setScene(scene);
+        stage.show();
+    }
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
